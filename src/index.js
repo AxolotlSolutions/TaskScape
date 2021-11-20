12746123c92cc882a5f5dbd1900b3f9d8c9cd486
mikemasatsugu <mike.masatsugu@gmail.com>
2021-11-20 08:29:16 -1000

    Initial webpack and express server initialized

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+const Index = () => {
+  return <div>Welcome to Our Scratch Project!</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "main.js"
+  },
+  mode: process.env.NODE_ENV || 'development',
+  plugins: [htmlPlugin],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader",
+        options: { name: '/static/[name].[ext]' }
+      }
+    ]
+  },
+  // If CORS process blocks HTTP requests from front-end, look at changing proxy property here OR add permmissions in router's res.header properties
+  devServer: {
+    static: {
+      publicPath: path.resolve(__dirname, "dist")
+    },
+    port: 8080,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
+};
