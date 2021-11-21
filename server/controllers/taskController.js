0be5a93e1e80c9397ba2d98b0f7ce25d8b5cb069
mikemasatsugu <mike.masatsugu@gmail.com>
2021-11-20 17:40:33 -1000

    wireframe task and rewards routes

---
+++ server/controllers/taskController.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const taskController = {}
+
+taskController.addTask = (req, res, next) => {
+
+};
+
+taskController.getTask = (req, res, next) => {
+
+};
+
+taskController.deleteTask = (req, res, next) => {
+
+};
+
+// Stretch features
+taskController.updateTask = (req, res, next) => {
+  // 
+};
+
+module.exports = taskController;
---
+++ server/routes/rewards.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const rewardsController = {};
+
+rewardsController.getRewards = (req, res, next) => { };
+
+rewardsController.addRewards = (req, res, next) => { };
+
+rewardsController.deleteRewards = (req, res, next) => { };
+
+module.exports = rewardsController;
